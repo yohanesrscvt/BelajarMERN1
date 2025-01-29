0e40d3e5b921d5e7031ea561e1c8b0585267ee1c
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2025-01-29 17:06:30 +0700

    init connect to mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const workoutRoutes = require('./routes/workouts');
 
 // express app
@@ -14,9 +15,28 @@ app.use((req, res, next) => {
     next();
 });
 
+// routes
 app.use('/api/workouts',workoutRoutes);
 
-// listen for request
-app.listen(process.env.PORT, () =>{
-    console.log('listening on port '+process.env.PORT+'!!!');
+// connect to db
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONG_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("sample_mflisx").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
